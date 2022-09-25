abfb707f2b767fe9be71cab13ecb0b7209d6e391
feehanjt <feehanjt@wustl.edu>
2022-09-25 02:36:52 -0500

    Making some changes to database stuff

--- app.py
+++ app.py
@@ -9,8 +9,14 @@ app = Flask(__name__)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 db = myclient["PoolRankings"]
-records = db.get_collection('Records')
 
+# if(db.get_collection("Records")):
+#     records = db.get_collection('Records')
+# else:
+
+records = db.Records
+result = records.insert_one({"Name": "Jonathan", "Rating": constants.StartingRating, "Matches": "0" })
+print(result)
 
 @app.route('/')
 def hello():
@@ -26,14 +32,19 @@ def addNewPlayer():
     nameToBeAdded = data['PlayerName']
 
 
-    #checks to see if name exists in database already
-    if records.find({}):
-        for record in records.find({}):
-            if record['Name'] == nameToBeAdded:
-                #fix error code
-                return 'false', 418
-                
-    db.records.inset({"Name": nameToBeAdded, "Rating": constants.StartingRating, "Matches": "0" })
+    # checks to see if name exists in database already
+    # if records.find({}):
+    #     print("in If statement")
+    #     for record in records.find({}):
+    #         print("in for loop")
+    #         print(record)
+    #         if record['Name'] == nameToBeAdded:
+    #             #fix error code
+    #             print("name found")
+    #             return 'false', 418
+    print("just before insert")   
+    db.records.insert_one({"Name": nameToBeAdded, "Rating": constants.StartingRating, "Matches": "0" })
+    print("At the end")
     return 'done', 201
 
 
