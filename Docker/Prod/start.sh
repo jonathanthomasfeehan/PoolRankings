c822977614b48442e1f941012a3dca319e3f5b86
Feehanjt <feehanjt@wustl.edu>
2025-05-03 09:42:28 -0500

    Updating docker entry point to give access to secrets

---
+++ Docker/Prod/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -euo pipefail
+
+echo "Fixing permissions for secrets..."
+
+chown appuser:appuser /etc/secrets/poolrankings-firebase-adminsdk-fbsvc-d688c2a06a.json
+chmod 400 /etc/secrets/*.json
+
+echo "Starting Gunicorn server..."
+exec su appuser -c "gunicorn -w 4 -b 0.0.0.0:5000 app:app"
